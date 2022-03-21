2166ec68e017d8ce877e4f3ce3426eb6562d0337
Cicero Rodrigues <99734384+cicero-rodrigues-neto@users.noreply.github.com>
2022-03-21 15:09:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let met = 3.5;
+let massaMagra = "";
+let minutos = 1440;
+let litros = 1000;
+let oxigenio = 5;
+let calculo = (met * massaMagra * minutos / litros * oxigenio)
+
+
+console.log(calculo)
+document.getElementById("butao").onclick
